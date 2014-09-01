4cc61e5f5c8c57cdb1272766874a549afabaa1a2
Niclas Hedhman <niclas@hedhman.org>
2014-09-01 16:26:52 +0800

    Maven setup script. To be _source_ into the current console. For my computer only.

---
+++ setupMaven.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export PATH=$PATH:/Users/niclas/Applications/apache-maven-3.2.3/bin
+export JAVA_HOME=/Library//Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home
+export M2_HOME=/Users/niclas/Applications/apache-maven-3.2.3/
+
